263e75eaea925a216f5dd43cd8955970e4d4671f
Miguel Peixe Aldeias <hello@migloo.be>
2021-04-15 09:27:47 +0200

    Adding missings libs & sass loader just in case

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,10 @@ import PropTypes from 'prop-types';
 
 import theme from '_comps/Theme';
 
+import 'swiper/swiper.scss';
+import 'swiper/components/scrollbar/scrollbar.scss';
+import 'swiper/components/navigation/navigation.scss';
+
 const MyApp = ({ Component, pageProps }) => (
 	<ChakraProvider theme={theme}>
 		<Component {...pageProps} />
